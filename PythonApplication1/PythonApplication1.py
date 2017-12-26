7a1230e0aa13e82b28f35bda991390c826601603
usmanics <usmanics@ymail.com>
2017-12-26 09:54:01 +0500

    c

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,3 +2,9 @@ import tensorflow as tf
 import numpy as np
 
 from tensorflow.examples.tutorials.mnist import input_data
+sess = tf.InteractiveSession()
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+sess.run(tf.global_variables_initializer())
+y = tf.matmul(x,W) + b
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y))
